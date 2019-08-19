eb8f36e18e8ee39309238233ec5d3e7cc13d322c
hometinker12 <48232506+hometinker12@users.noreply.github.com>
2019-08-18 21:44:25 -0400

    Update simple_server.ino

--- examples/simple_server/simple_server.ino
+++ examples/simple_server/simple_server.ino
@@ -6,8 +6,13 @@
 //
 
 #include <Arduino.h>
+#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
+#else
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
